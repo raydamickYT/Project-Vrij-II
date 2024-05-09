de584ebf1522bd32e575341424b0d7d2535d37ae
Adam Bausch <Fireworksanimations21@gmail.com>
2024-05-10 00:51:51 +0200

    wat web  dingen

--- NodeJS/First_Server/server.js
+++ NodeJS/First_Server/server.js
@@ -1,25 +1,50 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const port = 3000;
+
+// // Statische bestanden serveren
+// app.use(express.static('public'));
+
+// // Route voor de hoofdpagina
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/public/index.html'));
+// });
+// app.get('/about', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/public/about.html'));
+// });
+
+// // Server starten
+// app.listen(port, () => {
+//   console.log(`Server luistert op http://localhost:${port}`);
+// });
+
 const express = require('express');
-const path = require('path');
+const http = require('http');
+const socketIo = require('socket.io');
+
 const app = express();
-const port = 3001; // Je kunt elke poort kiezen
+const server = http.createServer(app);
+const io = socketIo(server);
 
-app.get('/', (req, res) => {
-  res.send('Hallo Wereld!');
-});
+const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server luistert op http://localhost:${port}`);
-});
-
-// Statische bestanden serveren
 app.use(express.static('public'));
 
-// Route voor de hoofdpagina
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+io.on('connection', (socket) => {
+  console.log('Een gebruiker is verbonden');
+
+  socket.on('disconnect', () => {
+    console.log('Een gebruiker is losgekoppeld');
+  });
+
+  socket.on('message', (msg) => {
+    console.log('Bericht ontvangen:', msg);
+    io.emit('message', msg); // Echo het bericht naar alle clients
+  });
 });
 
-// Server starten
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server luistert op http://localhost:${port}`);
 });
+
