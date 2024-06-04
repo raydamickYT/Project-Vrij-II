837c65c6878e710c588fcbc6a94e2ade2a8aeb62
AdamLaptop <fireworksanimations21@gmail.com>
2024-06-04 16:58:32 +0200

    minigames
    scenes laden en unloaden:
    doe je door de juiste path te kopieren
    te doen:
    unity laten zeggen wat er moet gebeuren

--- Unity/MiniGamesUnityProject/MiniGames/Assets/Scripts/Managers/StartScreenManager.cs
+++ Unity/MiniGamesUnityProject/MiniGames/Assets/Scripts/Managers/StartScreenManager.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class StartScreen : MonoBehaviour
 {
+    public string CurrentSceneName, NextSceneName;
     public void OnStartButtonClicked()
     {
         GameManager.Instance.OnStartButtonPressed();
+    }
+
+    public void OnButtonPressed()
+    {
+        GameManager.Instance.OnButtonPressed(CurrentSceneName, NextSceneName);
     }
 }
