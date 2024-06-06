11e5e3c9b12cc29469c08f31ee38f8ad7725ca09
Joystick <j.de.wekker@gmail.com>
2024-06-06 14:50:32 +0200

    Code voor veranderen sprite

---
+++ Unity/Project vrij 2/Assets/_Vrij/TriggerChangePlayerAnimation.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class TriggerChangePlayerAnimation : MonoBehaviour
+{
+    // The player GameObject
+    public GameObject player;
+
+    // Animation state names for normal and collided states
+    public string normalAnimation;
+    public string collidedAnimation;
+
+
+    // Reference to the Animator component on the player
+    private Animator playerAnimator;
+
+    // To track if the player's animation is currently in the collided state
+    private bool isCollided = false;
+
+    void Start()
+    {
+        if (player != null)
+        {
+            // Get the Animator component from the player GameObject
+            playerAnimator = player.GetComponent<Animator>();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the colliding object is the player
+        if (other.gameObject == player)
+        {
+            // Toggle the collided state
+            isCollided = !isCollided;
+
+            // Switch player's animation state
+            if (playerAnimator != null)
+            {
+                if (isCollided)
+                {
+                    playerAnimator.Play(collidedAnimation);
+                }
+                else
+                {
+                    playerAnimator.Play(normalAnimation);
+                }
+            }
+        }
+    }
+}
+
