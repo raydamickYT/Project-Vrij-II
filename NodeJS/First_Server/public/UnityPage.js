04c7f20c8dcc3554b6944d6b159f78262808810b
Adam <Fireworksanimations21@gmail.com>
2024-06-07 17:00:42 +0200

    more minigames

--- NodeJS/First_Server/public/UnityPage.js
+++ NodeJS/First_Server/public/UnityPage.js
@@ -24,7 +24,7 @@ function sendMessageToUnity(message) {
 // Unity WebGL Loader 
 function Unity() {
     var buildUrl = "/UnityBuild"; // Update the path to match your server setup
-    var version = "v11"; // Update the version as needed
+    var version = "v13"; // Update the version as needed
     var loaderUrl = buildUrl + "/Build/Build_" + version + ".loader.js";
     var config = {
         dataUrl: buildUrl + "/Build/Build_" + version + ".data",
