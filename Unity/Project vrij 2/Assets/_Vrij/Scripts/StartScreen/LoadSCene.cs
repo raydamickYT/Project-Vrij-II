2d31e4651739efc6ba0bb67b189937975f4aeda1
AdamLaptop <fireworksanimations21@gmail.com>
2024-06-08 22:46:53 +0200

    laatste dingetjes voor vandaag (i'm dying)

---
+++ Unity/Project vrij 2/Assets/_Vrij/Scripts/StartScreen/LoadSCene.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSCene : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SceneManager.LoadScene("Bos");
+
+        }
+    }
+
+}
--- Unity/Project vrij 2/Assets/_Vrij/Scripts/Triggers/CutsceneTrigger.cs
+++ Unity/Project vrij 2/Assets/_Vrij/Scripts/Triggers/CutsceneTrigger.cs
@@ -33,6 +33,7 @@ public class CutsceneTrigger : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             PlayCutscene();
+            Debug.Log("Hoi");
         }
     }
 
